047661b15e2f111ee341dd2dfdcc6742c4eafa49
Randal Michnovicz <rmichnovicz@gatech.edu>
2017-02-21 15:44:28 -0500

    Add user type spinner

--- app/src/main/java/edu/gatech/robodroids/raindrop/UserModel.java
+++ app/src/main/java/edu/gatech/robodroids/raindrop/UserModel.java
@@ -8,15 +8,21 @@ public class UserModel {
     public String name;
     public String userid;
     public String pass;
+    public String type;
 
     public UserModel() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public UserModel(String name, String userid, String pass) {
+    public UserModel(String name, String userid, String pass, String type) {
         this.name = name;
         this.userid = userid;
         this.pass = pass;
+        this.type = type;
+    }
+
+    public UserModel(String name, String userid, String pass) {
+        this(name, userid, pass, "User");
     }
 
 }
